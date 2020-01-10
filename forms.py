e561b840f052cc6d1491f4dc50d63d9e7d83b979
coldice1915 <coldice.zs44@gmail.com>
2020-01-10 15:14:15 -0800

    add new fields to ArtistForm

--- forms.py
+++ forms.py
@@ -221,5 +221,10 @@ class ArtistForm(Form):
         # TODO implement enum restriction
         'facebook_link', validators=[URL()]
     )
+    website = StringField(
+        'website', validators=[URL()]
+    )
+    seeking_venue = BooleanField('seeking_venue')
+    seeking_description = StringField('seeking_description')
 
 # TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
