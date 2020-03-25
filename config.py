383ec112e409cfa5554ac0fe8944b4968ba37440
coldice1915 <coldice.zs44@gmail.com>
2020-03-24 18:03:04 -0700

    change local user

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgres://{}@{}/{}'.format(
-    'jameshan', 'localhost', 'fyyur')
+    'socom', 'localhost', 'fyyur')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
